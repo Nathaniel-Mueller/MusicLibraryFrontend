248f1a3315a95b6663f4f30a259bf1a25dc4fb21
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-12-07 13:14:00 -0800

    Imported Axios, created getSongs funtion and useEffect function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getSongs()
+  }, []);
+
+  async function getSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/Music/')
+    console.log(response.data)
+    setSongs(response.data)
+  }
+
   return (
     <div>
-    
+      <button onClick={() => getSongs()}>Get all Songs</button>
     </div>
   );
 }
