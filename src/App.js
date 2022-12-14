ad8d2a4836b8a36cdd1c65b5608ef388bd575dc8
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-12-14 12:31:53 -0800

    Added initial findSong functionality

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ function App() {
   }, []);
 
   async function getSongs(){
-    const response = await axios.get('http://127.0.0.1:8000/api/Music/')
+    const response = await axios.get('http://127.0.0.1:8000/api/music/')
     console.log(response.data)
     setSongs(response.data)
+  }
+
+  async function findSong(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/3/')
+    setSongs(response.data)
+    console.log(response.data)
   }
 
   return (
     <div>
       <button onClick={() => getSongs()}>Get all Songs</button>
+      <button onClick={()=> findSong()}>test</button>
     </div>
   );
 }
