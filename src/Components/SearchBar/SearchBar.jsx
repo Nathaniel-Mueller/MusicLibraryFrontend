50afad41ef2dc6a3e533dd1feb44500e9866e4b5
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2023-01-13 20:30:54 -0800

    changed release date type from text to date

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -29,7 +29,7 @@ const SearchBar = (props) => {
                 <input className='spacing' type='text' value={title} placeholder='Title' onChange = {(event) => setTitle(event.target.value)}/>
                 <input className='spacing' type='text' value={artist} placeholder = 'Artist' onChange = {(event) => setArtist(event.target.value)}/>
                 <input className='spacing' type='text' value={album} placeholder = 'Album' onChange = {(event) => setAlbum(event.target.value)}/>
-                <input className='spacing' type='text' value={releaseDate} placeholder='Release Date (YYYY-MM-DD)' onChange = {(event) => setReleaseDate(event.target.value)}/>
+                <input className='spacing' type='date' value={releaseDate} placeholder='Release Date (YYYY-MM-DD)' onChange = {(event) => setReleaseDate(event.target.value)}/>
                 <input className='spacing' type='text' value={genre} placeholder = 'Genre' onChange = {(event) => setGenre(event.target.value)}/>
                 <button className='spacing' type='submit'>Search</button>
             </div>
